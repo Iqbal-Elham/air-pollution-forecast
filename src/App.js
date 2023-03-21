d38980d18e8a8ec7649741d1d4eaf82ced842383
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-21 10:28:11 +0430

    Add react routes in app file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Rockets from './components/Rockets';
-import Missions from './routes/Missions';
-import Profile from './routes/Profile';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navbar />}>
-          <Route index element={<Rockets />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/my-profile" element={<Profile />} />
+        <Route path="/" element={<h1>hello home</h1>}>
+          <Route index element={<h2>hello home page</h2>} />
+          <Route path="/details" element={<h1>Hello details</h1>} />
           <Route path="*" element={<div>Page not found</div>} />
         </Route>
       </Routes>
